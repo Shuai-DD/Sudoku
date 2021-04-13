550428677aafb2f815fd8afe97d1ddfb6d2a2bab
Shuai-DD <1367558996@qq.com>
2021-04-13 22:12:50 +0800

    able to generate unique base grids and set a difficulty.

--- Sudoku/Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku/Sudoku.cpp
@@ -1,8 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include"Sudoku.h"
 #include<time.h>
 #include<cstdlib>
 #include<iostream>
 #include<fstream>
+#include<stdio.h>
 using namespace std;
 
 SudokuGrids::SudokuGrids()
@@ -39,12 +41,39 @@ bool Sudoku::JudgeLegitimacy(SudokuGrids* sudoku_grid, int row, int col, int val
 {
 	for (int i = 0; i < col; i++)
 	{
-		if (val == sudoku_grid->suduku_grid[row][col])
+		if (val == sudoku_grid->suduku_grid[row][i])
 		{
 			return false;
 		}
 	}
 	for (int i = 0; i < row; i++)
+	{
+		if (val == sudoku_grid->suduku_grid[i][col])
+		{
+			return false;
+		}
+	}
+	int count = col % 3 + row % 3 * 3;
+	while (count--)
+	{
+		if (val == sudoku_grid->suduku_grid[row / 3 * 3 + count / 3][col / 3 * 3 + count % 3])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool Sudoku::JudgeLegitimacy1(SudokuGrids* sudoku_grid, int row, int col, int val)
+{
+	for (int i = 0; i < 9; i++)
+	{
+		if (val == sudoku_grid->suduku_grid[row][i])
+		{
+			return false;
+		}
+	}
+	for (int i = 0; i < 9; i++)
 	{
 		if (val == sudoku_grid->suduku_grid[i][col])
 		{
@@ -122,13 +151,12 @@ void Sudoku::GenSolutionGrid(int grid_num)
 		temp_grid = next_grid;
 		
 	}
-	/*temp_grid = sudoku_grids_;
-	while (temp_grid)
-	{
-		temp_grid->print();
-		temp_grid = temp_grid->next_grid;
-	}*/
-	
+	temp_grid = sudoku_grids_;
+	//while (temp_grid)
+	//{
+	//	//temp_grid->print();
+	//	temp_grid = temp_grid->next_grid;
+	//}
 }
 
 void Sudoku::GenBaseGrid(int lower_limit, int upper_limit)
@@ -172,9 +200,9 @@ void Sudoku::GenBaseGrid(int lower_limit, int upper_limit)
 	}
 }
 
-void Sudoku::GridWritter()
+void Sudoku::GridWritter(char* writter_path)
 {
-	ofstream writter("solution.txt");
+	ofstream writter(writter_path);
 	SudokuGrids* temp_grid = sudoku_grids_;
 	if (writter.is_open())
 	{
@@ -193,11 +221,163 @@ void Sudoku::GridWritter()
 		}
 	}
 	temp_grid = sudoku_grids_;
-	while (temp_grid)
-	{
-		temp_grid->print();
-		temp_grid = temp_grid->next_grid;
-	}
+	//while (temp_grid)
+	//{
+	//	temp_grid->print();
+	//	temp_grid = temp_grid->next_grid;
+	//}
 
 }
 
+void Sudoku::GridReader(char* reader_path)
+{
+	char all_grids[16000];//用数组存整个txt文档
+	int length = 0;//数组长度
+	FILE* reader;
+	reader = fopen(reader_path, "r");
+	if (reader == NULL)return;
+	while (1) 
+	{
+		char c;
+		c = fgetc(reader);
+		if (c == EOF) break;
+		all_grids [length] = c;
+		length++;
+	}
+	fclose(reader);
+	//读取文件
+	SudokuGrids* temp_grid = new SudokuGrids;
+	SudokuGrids* new_grid = new SudokuGrids;
+	grid_num_ = 0;
+	//for (int i = 0; i <= length; i++)cout << all_grids[i];
+	int tail = 0;//数组下标
+	int n = 0;
+
+	while(tail<length)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				temp_grid->suduku_grid[i][j] = all_grids[tail] - '0';
+				tail += 2;
+			}
+			tail++;
+		}
+		tail++;
+		if (grid_num_ == 0)
+		{
+			temp_grid->next_grid = new_grid;
+			sudoku_grids_ = temp_grid;
+			temp_grid = temp_grid->next_grid;
+			grid_num_++;
+			continue;
+		}
+		new_grid = new SudokuGrids;
+		temp_grid->next_grid = new_grid;
+		temp_grid = temp_grid->next_grid;
+		grid_num_++;
+	}
+}
+
+void Sudoku::BackTrace(SudokuGrids* sudoku_grid, int n)
+{
+	if (n == 81)
+	{
+		sudoku_grid->solution_sign = 1;
+		return;
+	}
+	if (sudoku_grid->suduku_grid[n / 9][n % 9] != 0)
+	{
+		BackTrace(sudoku_grid, n + 1);
+	}
+	if (sudoku_grid->suduku_grid[n / 9][n % 9] == 0)
+	{
+		for (int i = 1; i <= 9; i++)
+		{
+			if (JudgeLegitimacy1(sudoku_grid, n / 9, n % 9, i))
+			{
+				sudoku_grid->suduku_grid[n / 9][n % 9] = i;
+				BackTrace(sudoku_grid, n + 1);
+				if (sudoku_grid->solution_sign == 1)
+				{
+					return;
+				}
+				sudoku_grid->suduku_grid[n / 9][n % 9] = 0;
+			}
+		}
+	}
+}
+
+void Sudoku::BackTrace1(SudokuGrids* sudoku_grid, int n)
+{
+	if (n == 81)
+	{
+		sudoku_grid->solutions++;
+		sudoku_grid->solution_sign = 1;
+		return;
+	}
+	if (sudoku_grid->suduku_grid[n / 9][n % 9] != 0)
+	{
+		BackTrace1(sudoku_grid, n + 1);
+	}
+	if (sudoku_grid->suduku_grid[n / 9][n % 9] == 0)
+	{
+		for (int i = 1; i <= 9; i++)
+		{
+			if (JudgeLegitimacy1(sudoku_grid, n / 9, n % 9, i))
+			{
+				sudoku_grid->suduku_grid[n / 9][n % 9] = i;
+				BackTrace1(sudoku_grid, n + 1);
+				sudoku_grid->suduku_grid[n / 9][n % 9] = 0;
+			}
+		}
+	}
+}
+
+void Sudoku::Solution()
+{
+	BackTrace(sudoku_grids_, 0);
+	//sudoku_grids_->print();
+	for (int i = 0; i < grid_num_ - 1; i++) {
+		sudoku_grids_ = sudoku_grids_->next_grid;
+		BackTrace(sudoku_grids_, 0);
+		//sudoku_grids_->print();
+	}
+}
+
+void Sudoku::GenUniqeBaseGrid(int grid_num)
+{
+	int gened_grid_num = 0;
+	SudokuGrids* temp_grid = new SudokuGrids;
+	SudokuGrids* result = new SudokuGrids;
+	SudokuGrids* new_grid = new SudokuGrids;
+	while (gened_grid_num != grid_num)
+	{
+		GenSolutionGrid(1);
+		GenBaseGrid(20, 21);
+		//sudoku_grids_->print();
+		BackTrace1(sudoku_grids_,0);
+		//sudoku_grids_->print();
+		cout << sudoku_grids_->solutions << endl;
+		if (sudoku_grids_->solutions > 1 || sudoku_grids_->solution_sign == 0)
+		{
+			continue;
+		}
+		if (gened_grid_num == 0)
+		{
+			temp_grid = sudoku_grids_;
+			temp_grid->next_grid = new_grid;
+			result = temp_grid;
+			gened_grid_num++;
+			continue;
+		}
+		new_grid = sudoku_grids_;
+		temp_grid->next_grid = new_grid;
+		temp_grid = temp_grid->next_grid;
+		new_grid = new SudokuGrids;
+		gened_grid_num++;
+	}
+	grid_num_ = grid_num;
+	sudoku_grids_=result;
+}
